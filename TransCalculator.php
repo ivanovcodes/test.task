63ea363b157f37f428a32811bf7feb7eb1087472
ivanovsite <development@ivanov.site>
2017-08-29 15:18:23 +0300

    deploy TransCalculator

---
+++ TransCalculator.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Тестовое задание # 1
+ * Расчет стоимости доставки груза по перевозчикам.
+ * Архитектура подразумевает рост количества перевозчиков со временем и добавление новых алгоритмов расчета другими программистами.
+ *
+ * @author Иванов Владимир <development@ivanov.site>
+ *
+ */
+
+class TransCalculator
+{
+	private $post;
+
+	public function setPost(TransInterface $post)
+	{
+		$this->post = $post;
+	}
+
+	public function calculate($weight)
+	{
+		//$weight++;
+		return $this->post->calculate($weight);
+	}
+}
+
+interface TransInterface
+{
+	public function calculate($weight);
+}
+
+class PostRussia implements TransInterface 
+{
+	public function calculate($weight){
+		return $weight + 1;
+	}
+}
+
+class PostDHL implements TransInterface 
+{
+	public function calculate($weight){
+		return $weight - 1;
+	}
+}
