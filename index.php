04fbf1bfdd1016600a5c2cf471ea8a9d2efe30e4
ivanovsite <development@ivanov.site>
2017-08-30 16:41:27 +0300

    fix author

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Тестовое задание # 2
  * Работа с подписчиками одного издания.
  *
- * @see https://github.com/ivanovsite/test.task/tree/TransCalculator
+ * @see https://github.com/ivanovsite/test.task/tree/BookEdition
  * @author Иванов Владимир <development@ivanov.site> 
  *
  */
@@ -16,13 +16,14 @@ mysqli_query($db, 'set names utf8');
 mysqli_query($db, "SET sql_mode = ''");
 
 $result = mysqli_query($db, "
-	SELECT tbl_editions.title, COUNT(*) AS readers FROM tbl_subscriptions LEFT JOIN tbl_editions ON tbl_editions.id = tbl_subscriptions.id_edition WHERE tbl_editions.title LIKE '%".$edition."%' GROUP BY tbl_subscriptions.id_edition LIMIT 1;
+	SELECT tbl_editions.title, COUNT(*) AS readers, DATEDIFF(YEAR, tbl_readers.birthday, NOW()) AS age
+ FROM tbl_subscriptions LEFT JOIN tbl_editions ON tbl_editions.id = tbl_subscriptions.id_edition LEFT JOIN tbl_readers ON tbl_readers.id = tbl_subscriptions.id_reader WHERE tbl_editions.title LIKE '%".$edition."%' GROUP BY tbl_subscriptions.id_edition LIMIT 1;
 ");
 $row = mysqli_fetch_array($result);
 print_r('Всего на журнал: '.$row['title'].' подписано '.$row['readers'].' читателей');
 
 $result = mysqli_query($db, "
-	SELECT tbl_readers.id, tbl_readers.name FROM tbl_readers JOIN(SELECT RAND() * (SELECT MAX(id) FROM tbl_readers) AS last_id) AS r WHERE tbl_readers.id >= r.last_id ORDER BY tbl_readers.id ASC LIMIT 1;
+	SELECT tbl_readers.id, tbl_readers.name FROM tbl_reader JOIN(SELECT RAND() * (SELECT MAX(id) FROM tbl_readers) AS last_id) AS r WHERE tbl_readers.id >= r.last_id ORDER BY tbl_readers.id ASC LIMIT 1;
 ");
 $row = mysqli_fetch_array($result);
 print_r(', а это наш любимый читатель: '.$row['name'].' [#'.$row['id'].']');
